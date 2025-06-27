441964e847f090125d57a7908372ce1a9de91ec7
Samra Ateeque <samraateeque12@gmail.com>
2025-06-27 22:03:24 +0530

    fix:cors

--- BackEnd/src/routes/ai.routes.js
+++ BackEnd/src/routes/ai.routes.js
@@ -4,6 +4,7 @@ const aiController = require("../controllers/ai.controller")
 const router = express.Router();
 
 
+router.post('/get-review', aiController.getReview);
 
 
 module.exports = router;    
