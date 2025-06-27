98a2dbb3eee33abfcf636bd59588183de1085466
Samra Ateeque <samraateeque12@gmail.com>
2025-06-27 21:53:58 +0530

    fix:cors

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -4,6 +4,14 @@ const cors = require('cors')
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://code-reviewer-1-8afe.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET,POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+
 app.use(cors({
   origin: 'https://code-reviewer-1-8afe.onrender.com', // your actual frontend URL
     methods: ["GET", "POST"],
